5fe8a1b03206a46ee71119452bf0c8c1fc55d9a1
rsta <rsta@umich.edu>
2024-07-25 12:23:18 -0400

    Changes to the schema and entities

--- internal/entities/hyperquack.go
+++ internal/entities/hyperquack.go
@@ -1,3 +1,37 @@
 package entities
 
-type Hyperquack struct{}
+import "time"
+
+type Hyperquack struct{
+
+	protocol string
+	domain string
+	domainIsControl bool
+	date time.Time
+	startTime time.Time
+	endTime time.Time
+	serverIp string
+	serverNetblock string
+	serverAsn string
+	serverAsName string
+	serverAsFullName string
+	serverAsClass string
+	server_country string
+	serverOrganization string
+	source string
+	receivedError string
+	receivedStatus string
+	receivedHeaders string
+	receivedBody string
+	receivedTlsVersion string
+	ReceviedTlsCipherSuite string
+	ReceivedTlsCert string
+	ReceivedTlsCertCommonName string
+	ReceivedTlsCertAlternativeNames string
+	ReceivedTlsCertIssuer string
+	matchesTemplate bool
+	noResponseInMeasurementMatchesTemplate bool
+	controlsFailed bool
+	statefulBlock bool
+
+}
--- internal/entities/satellite.go
+++ internal/entities/satellite.go
@@ -1,3 +1,27 @@
 package entities
 
-type Satellite struct{}
+import "time"
+
+type Satellite struct{
+
+  domain string
+  domainIsControl bool
+  testUrl string
+  date time.Time
+  startTime time.Time
+  endTime time.Time
+  resolverIp string
+  resolverName string
+  resolverIsTrusted bool
+  resolverNetblock string
+  resolverAsn string
+  resolverAsName string
+  resolverAsFullName string 
+  resolverAsClass string
+  resolverCountry string
+  resolverOrganization string
+  receivedError string
+  receivedRcode int
+  source string
+
+}
